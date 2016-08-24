f0c2ac81f9a757472e6a30b5dc198d977ce789e5
sridh <sridh@HP-DESKTOP-SSD>
2016-08-24 16:12:19 -0400

    Add project files.

---
+++ MSProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSProject.Startup))]
+namespace MSProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
